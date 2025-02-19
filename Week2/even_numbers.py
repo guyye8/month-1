44f667912097cd7fcafa9709903f2de6211bc68d
guyye8 <guyasau@gmail.com>
2025-02-19 00:32:54 -1200

    Update even_numbers.py

--- Week2/even_numbers.py
+++ Week2/even_numbers.py
@@ -1,1 +1,7 @@
-
+for num in range(21):
+    if num == 0 :
+        print(f'{num} is neither even nor odd.')
+    elif num % 2 == 0:
+        print(f'{num} is an even number.')
+    else:
+        print(f'{num} is an odd number.')
