06d7bcb7b418f97e3fabd27faa65ed8ad2e7b4de
guyye8 <guyasau@gmail.com>
2025-02-19 00:35:22 -1200

    Update largest_num.py

--- Week2/largest_num.py
+++ Week2/largest_num.py
@@ -1,1 +1,14 @@
+import random
 
+num_list = [random.randint(0, 100) for _ in range(10)]
+
+print("Generated List:", num_list)
+
+# Find the largest number
+largest = float('-inf')  # Initialize with the smallest possible value
+
+for num in num_list:
+    if num > largest:  # Update largest if a bigger number is found
+        largest = num
+
+print(f'The largest number is: {largest}')
