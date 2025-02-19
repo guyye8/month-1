dcf57e2eb7b28bd3fcb82234e44c256b98aae5b6
guyye8 <guyasau@gmail.com>
2025-02-19 00:30:20 -1200

    Update sum_list.py

--- Week2/sum_list.py
+++ Week2/sum_list.py
@@ -1,1 +1,17 @@
+import random
+num_list =[]
 
+for x in range (0,9):
+    num = random.randint(0,4)
+    num_list.append(num)
+
+
+def sum():
+    prv = 0
+    for num in num_list:
+        new = num
+        total = prv + new
+        prv = total
+    print(f'the total sum is {total}')
+
+sum()
